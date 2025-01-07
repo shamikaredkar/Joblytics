7a5bd66ae93b0d2184dcb0bcf4a5ab57abb0e889
Shamika Redkar <shamikaredkar91@gmail.com>
2025-01-07 14:26:31 -0500

    Created basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <LandingPage />
     </>
   );
 }
