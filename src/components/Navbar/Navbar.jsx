f62379cbdda28879bdc9c8764f4bbe242cf9d6b4
Shamika Redkar <shamikaredkar91@gmail.com>
2025-01-08 10:14:25 -0500

    Installed packages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,13 +25,13 @@ const Navbar = () => {
           <div className='flex md:order-2 space-x-3'>
             <button
               type='button'
-              className='text-gray-700 font-bold hover:text-blue-700 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2'
+              className='text-gray-700 font-bold hover:text-blue-700 font-medium rounded-lg text-sm px-4 py-2'
             >
               Sign In
             </button>
             <button
               type='button'
-              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2'
+              className='text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2'
             >
               Get Started
             </button>
